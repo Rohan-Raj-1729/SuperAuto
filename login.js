ec0cce2160146dfdca9ca9158a7ab1e2ccafdaac
Rohan-Raj-1729 <mahajanrohanraj@gmail.com>
2021-06-18 21:10:06 +0530

    Login Page bugs

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
 function validate()
 {
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    if(username="admin"&& password="user")
-    {
-        alert("login suscesfully");
-        return false;
-    }
-    else
-    {
-        alert("login failed");
-    }
-
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="admin" && password=="user")
+{
+    alert("login successful");
+    return true;
+}
+else
+{
+    alert("login failed");
+    return false;
+}
 }
